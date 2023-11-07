fc0be9e08d50926718f43a3d30fee06f7c186263
Akozi Emmanuel <akoziemmanuel@gmail.com>
2023-11-07 10:26:15 +0100

    Zenith update

--- app/src/main/java/com/woleapp/netpos/contactless/ui/fragments/NewOrExistingFragment.kt
+++ app/src/main/java/com/woleapp/netpos/contactless/ui/fragments/NewOrExistingFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
 import com.woleapp.netpos.contactless.BuildConfig
@@ -17,7 +18,7 @@ import com.woleapp.netpos.contactless.util.RandomPurposeUtil
 import com.woleapp.netpos.contactless.util.RandomPurposeUtil.getDeviceId
 import com.woleapp.netpos.contactless.util.RandomPurposeUtil.initPartnerId
 import com.woleapp.netpos.contactless.util.RandomPurposeUtil.observeServerResponse
-import com.woleapp.netpos.contactless.util.RandomPurposeUtil.observeServerResponseOnce
+import com.woleapp.netpos.contactless.util.RandomPurposeUtil.showAlertDialog
 import com.woleapp.netpos.contactless.viewmodels.ContactlessRegViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import io.reactivex.Scheduler
@@ -33,7 +34,8 @@ class NewOrExistingFragment : BaseFragment() {
     private lateinit var loader: AlertDialog
     private lateinit var newPartnerId: String
     private lateinit var deviceSerialID: String
-    private lateinit var account : String
+    private lateinit var account: String
+
     @Inject
     lateinit var compositeDisposable: CompositeDisposable
 
@@ -52,118 +54,111 @@ class NewOrExistingFragment : BaseFragment() {
     ): View {
         binding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_new_or_existing, container, false)
+        // Handle Back Press
+        activity?.onBackPressedDispatcher?.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    requireActivity().supportFragmentManager.popBackStack()
+                }
+            }
+        )
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         newPartnerId = initPartnerId()
-        deviceSerialID = getDeviceId(requireContext()).toString()
+        deviceSerialID = getDeviceId(requireContext())
         viewModel.message.observe(viewLifecycleOwner) {
             it.getContentIfNotHandled()?.let { message ->
-                Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
-            }
-        }
-
-        if(BuildConfig.FLAVOR.contains("zenith")){
-            binding.newOrExistingCardview.visibility = View.GONE
-            binding.accountNumberCardview.visibility = View.VISIBLE
-
-            binding.dialogAccountNumberProceed.setOnClickListener {
-                binding.accountNumberCardview.visibility = View.VISIBLE
-                account = binding.dialogAccountNumberEditText.text.toString()
-                if (account.isNullOrEmpty()) {
-                    Toast.makeText(
-                        requireContext(),
-                        "Please, enter your account number",
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                } else if (account.length < 10) {
-                    Toast.makeText(
-                        requireContext(),
-                        "Please, enter valid account number",
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                } else {
-                    if (BuildConfig.FLAVOR.contains("firstbank") || BuildConfig.FLAVOR.contains("zenith")
-                        || BuildConfig.FLAVOR.contains("wemabank")) {
-                        viewModel.findAccountForFirstBankUser(account, newPartnerId, deviceSerialID)
-                        observeServerResponse(
-                            viewModel.firstBankAccountNumberResponse,
-                            loader,
-                            requireActivity().supportFragmentManager,
-                        ) {
-                            showFragment(
-                                ExistingCustomersRegistrationFragment(),
-                                containerViewId = R.id.auth_container,
-                                fragmentName = "ExistingCustomersRegistration Fragment",
-                            )
-                            viewModel.clearLiveData()
-                        }
-                    } else {
-                        otherBanks()
+                if (message == "OTP sent") {
+                    showAlertDialog(requireContext(), message, "OK") {
+                        showFragment(
+                            RegistrationOTPFragment(),
+                            containerViewId = R.id.auth_container,
+                            fragmentName = "RegisterOTP Fragment",
+                        )
                     }
+                } else {
+                    showAlertDialog(requireContext(), message, "OK") {}
                 }
             }
-
         }
+
         loader = RandomPurposeUtil.alertDialog(requireContext())
 
         binding.confirmationTvNo.setOnClickListener {
             showFragment(
                 RegisterFragment(),
                 containerViewId = R.id.auth_container,
-                fragmentName = "Register Fragment",
+                fragmentName = "Register Fragment"
             )
         }
 
         binding.confirmationTvYes.setOnClickListener {
-            activity?.getFragmentManager()?.popBackStack()
-            val inflater = LayoutInflater.from(requireContext())
-            val dialogView = inflater.inflate(R.layout.dialog_account_number_layout, null)
-            val dialogBuilder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
-            dialogBuilder.setView(dialogView)
+            enterAccountNumberDialog()
+        }
 
-            val alertDialog: AlertDialog = dialogBuilder.create()
-            alertDialog.show()
-            dialogView.findViewById<Button>(R.id.dialog_account_number_proceed).setOnClickListener {
-                alertDialog.dismiss()
-                account = dialogView.findViewById<EditText>(R.id.dialog_account_number_editText).text.toString()
-                if (account.isNullOrEmpty()) {
-                    Toast.makeText(
-                        requireContext(),
-                        "Please, enter your account number",
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                } else if (account.length < 10) {
-                    Toast.makeText(
-                        requireContext(),
-                        "Please, enter valid account number",
-                        Toast.LENGTH_SHORT,
-                    ).show()
-                } else {
-                    if (BuildConfig.FLAVOR.contains("firstbank") || BuildConfig.FLAVOR.contains("zenith")
-                        || BuildConfig.FLAVOR.contains("wemabank")) {
-                        viewModel.findAccountForFirstBankUser(account, newPartnerId, deviceSerialID)
-                        observeServerResponse(
-                            viewModel.firstBankAccountNumberResponse,
-                            loader,
-                            requireActivity().supportFragmentManager,
-                        ) {
-                            showFragment(
-                                ExistingCustomersRegistrationFragment(),
-                                containerViewId = R.id.auth_container,
-                                fragmentName = "ExistingCustomersRegistration Fragment",
-                            )
-                            viewModel.clearLiveData()
-                        }
-                    } else {
-                        otherBanks()
+        if (BuildConfig.FLAVOR.contains("zenith")) {
+            enterAccountNumberDialog()
+            binding.newOrExistingCardview.visibility = View.GONE
+        }
+        binding.enterAccountNumber.setOnClickListener {
+            enterAccountNumberDialog()
+        }
+    }
+
+    private fun enterAccountNumberDialog() {
+        activity?.getFragmentManager()?.popBackStack()
+        val inflater = LayoutInflater.from(requireContext())
+        val dialogView = inflater.inflate(R.layout.dialog_account_number_layout, null)
+        val dialogBuilder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+        dialogBuilder.setView(dialogView)
+
+        val alertDialog: AlertDialog = dialogBuilder.create()
+        alertDialog.show()
+        dialogView.findViewById<Button>(R.id.dialog_account_number_proceed).setOnClickListener {
+            alertDialog.dismiss()
+            account =
+                dialogView.findViewById<EditText>(R.id.dialog_account_number_editText).text.toString()
+            if (account.isNullOrEmpty()) {
+                Toast.makeText(
+                    requireContext(),
+                    "Please, enter your account number",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            } else if (account.length < 10) {
+                Toast.makeText(
+                    requireContext(),
+                    "Please, enter valid account number",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            } else {
+                if (BuildConfig.FLAVOR.contains("firstbank") || BuildConfig.FLAVOR.contains("zenith") || BuildConfig.FLAVOR.contains(
+                        "wemabank"
+                    )
+                ) {
+                    viewModel.findAccountForFirstBankUser(account, newPartnerId, deviceSerialID)
+                    observeServerResponse(
+                        viewModel.firstBankAccountNumberResponse,
+                        loader,
+                        requireActivity().supportFragmentManager,
+                    ) {
+//                            showFragment(
+//                                ExistingCustomersRegistrationFragment(),
+//                                containerViewId = R.id.auth_container,
+//                                fragmentName = "ExistingCustomersRegistration Fragment",
+//                            )
+                        viewModel.clearLiveData()
                     }
+                } else {
+                    otherBanks()
                 }
             }
         }
     }
+
 
     private fun otherBanks() {
         loader.show()
